2dca73c983f18df7ff41a94f3b85ea023c745368
Yashom Kapoor <yashom.kapoor@gmail.com>
2026-05-14 15:10:32 -0700

    try catch in install error

--- api/count.js
+++ api/count.js
@@ -5,7 +5,17 @@ const redis = Redis.fromEnv();
 export default async function handler(req, res) {
     const installs = await redis.get("installs");
 
-    res.status(200).json({
+    try {
+        const installs = await redis.get("installs");
+
+        res.status(200).json({
         installs: installs || 0
-    })
+        });
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            error: "Failed to fetch installs"
+        });
+    }
 }
